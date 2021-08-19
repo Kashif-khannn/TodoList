f8f89d06663013feebe3202d3c5696217ba5a61e
Kashif Khan <khankashif3019@gmail.com>
2021-08-19 16:46:04 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
-
+// importing Components
+import Form from "./components/Form";
 function App() {
   return (
     <div className="App">
       <header>
       <h1>Kashif's Todo List</h1>
       </header>
+      <Form/>
     </div>
   );
 }
